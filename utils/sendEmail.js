94206329930b32f9b407c5f0dddc5292996ae3d9
Radek Pleskac <info@radekpleskac.com>
2025-04-06 22:25:47 +0200

    update config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,6 +16,9 @@ export async function sendEmail(subject, message) {
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
